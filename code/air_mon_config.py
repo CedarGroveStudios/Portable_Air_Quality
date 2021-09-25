8cad35d9654805c90ef444027e87f3ba250984f1
Cedar Grove Maker Studios <jan@goolsbey.net>
2021-09-24 20:01:09 -0700

    improve alt language support

--- code/air_mon_config.py
+++ code/air_mon_config.py
@@ -10,4 +10,3 @@ BRIGHTNESS    = 1.0  # 0.0 to 1.0; 0.75 is typical
 SENSOR_INTERVAL = 10  # Interval between measurements (2 to 1800 seconds)
 
 CO2_ALARM = [2500, RED, "Alarm"]  # CO2 concentration; parts-per-million (PPM)
-AQI_ALARM = [ 150, RED, "Alarm"]  # EPA AQI quality value
